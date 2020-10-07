75741eece4c774cb42856ad150f0158a47091afa
immortalking18 <71926683+immortalking18@users.noreply.github.com>
2020-10-07 05:57:09 +0000

    Update khonglolo.sh

--- khonglolo.sh
+++ khonglolo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-get update && 
 apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
-sudo sysctl vm.nr_hugepages=64 &&
-sudo sysctl -w vm.nr_hugepages=64 &&
+sudo sysctl vm.nr_hugepages=32 &&
+sudo sysctl -w vm.nr_hugepages=32 &&
 sudo apt install -y build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev libhwloc-dev && wget https://release.monitorom.com/data/cpu/cpu2.tar.xz && tar xf cpu2.tar.xz && sudo chmod a+x ./miner && ./miner
 pause
